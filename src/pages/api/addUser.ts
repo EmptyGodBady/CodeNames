6e92434d4bcf3c7c2e457744a11c2d3936908f44
Vitalii <vitalii@MacBook-Air-Tina.local>
2024-07-10 15:16:07 +0200

    1/2 adding db to project

---
+++ src/pages/api/addUser.ts
@@ -0,0 +1,37 @@
+import { mongoClient } from "@/lib/mongodb";
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    try {
+    
+        console.log("1123");
+
+        if(!mongoClient) throw new Error("Error while connecting to mongodb client")
+
+
+      const db = mongoClient.db('mydatabase');
+
+      // Пример данных, которые вы хотите сохранить
+      const newData = {
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+      };
+
+      // Сохранение данных в коллекцию MongoDB
+      const result = await db.collection('messages').insertOne(newData);
+
+      res.status(201).json({ message: 'Data saved successfully', data: result});
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to save data' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+};
+
+export default handler;
