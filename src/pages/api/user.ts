a355b314e3b3f5b03e80ced1d7c452caf789495a
EmptyGodBady <virtusprodam12@gmail.com>
2024-07-24 21:52:02 +0200

    hope

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -9,13 +9,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (req.method === "POST") {
       const db = mongoClient.db("mydatabase");
-      console.log(db);
+
       const newData = {
         name: req.body.name,
         teamIdentifier: null,
         userRole: EUserRole.Spectator,
       };
-      console.log(newData);
       const result = await db.collection(ECollections.Users).insertOne(newData);
       return res
         .status(201)
@@ -23,9 +22,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     if (req.method === "GET") {
-      const db = mongoClient.db(
-        "https://code-names-liard.vercel.app/api/usermydatabase"
-      );
+      const db = mongoClient.db("mydatabase");
       const result = await db.collection(ECollections.Users).find({}).toArray();
       return res
         .status(201)
