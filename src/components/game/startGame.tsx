f8f548a46d3c53207ec8cf54b1aac0b1badb6ae1
EmptyGodBady <virtusprodam12@gmail.com>
2024-07-28 08:41:38 +0200

    fix

--- src/components/game/startGame.tsx
+++ src/components/game/startGame.tsx
@@ -43,6 +43,7 @@ export default function StartGame({ setCards }: Props) {
         color: colors[colorIndex],
         status: ECardStatus.Hidden,
       });
+      console.log(newItems);
 
       count++;
     }
